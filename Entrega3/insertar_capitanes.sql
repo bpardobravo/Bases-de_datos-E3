e70844a5baa35702feb9689c876c767db2bfbad8
Tomas <tavalenzuela@uc.cl>
2020-10-26 19:52:22 -0300

    se corrige csv jefes

--- Entrega3/insertar_capitanes.sql
+++ Entrega3/insertar_capitanes.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION insertar_capitanes ()
 RETURNS void AS
 $$
 BEGIN
-    INSERT INTO usuario SELECT nro_pasaporte, pnombre, genero, nacionalidad FROM personal
+    INSERT INTO usuario SELECT nro_pasaporte, pnombre, edad, genero, nacionalidad FROM personal
     WHERE es_capitan = 1;
 END
 $$ language plpgsql
