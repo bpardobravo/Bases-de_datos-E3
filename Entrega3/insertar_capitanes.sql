4a4b7b145d6177b365d69439e57598dec5efc25e
Tomas <tavalenzuela@uc.cl>
2020-10-26 22:41:23 -0300

    se actualiza procedimiento almacenado

--- Entrega3/insertar_capitanes.sql
+++ Entrega3/insertar_capitanes.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION insertar_capitanes ()
 RETURNS void AS
 $$
 BEGIN
-    INSERT INTO usuario SELECT nro_pasaporte, pnombre, edad, genero, nacionalidad FROM personal
+    INSERT INTO usuario (nro_pasaporte, unombre, edad, sexo, nacionalidad) SELECT nro_pasaporte, pnombre, edad, genero, nacionalidad FROM personal
     WHERE es_capitan = 1;
 END
 $$ language plpgsql
